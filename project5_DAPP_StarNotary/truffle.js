a7056c3923f7e6b661cf65c264dd90e5271b5cb7
k.f.gamehacker <k.f.gamehacker@gmail.com>
2018-12-03 18:23:07 +0800

    write StarNotary smart contracts

--- project5_DAPP_StarNotary/truffle.js
+++ project5_DAPP_StarNotary/truffle.js
@@ -13,6 +13,17 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: { 
+    development: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: "*"
+    }, 
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, infura),
+      network_id: 4,
+      gas : 6700000,
+      gasPrice : 10000000000
+    },
+  }
 };
